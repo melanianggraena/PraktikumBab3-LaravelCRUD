d9793ff8b0e1af8112f2152c7340016add660d67
Melani Anggraena <melanianggraena005@gmail.com>
2026-06-18 13:00:31 +0700

    Modul BAB3

--- app/Models/User.php
+++ app/Models/User.php
@@ -9,6 +9,7 @@ use Illuminate\Database\Eloquent\Attributes\Hidden;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use App\Enums\UserRole;
 
 #[Fillable(['name', 'email', 'password'])]
 #[Hidden(['password', 'remember_token'])]
@@ -22,11 +23,9 @@ class User extends Authenticatable
      *
      * @return array<string, string>
      */
-    protected function casts(): array
-    {
-        return [
-            'email_verified_at' => 'datetime',
-            'password' => 'hashed',
-        ];
-    }
+    protected $casts = [
+    'email_verified_at' => 'datetime',
+    'password'          => 'hashed',
+    'role'              => UserRole::class,
+    ];
 }
---
+++ resources/views/test.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.master')
+
+@section('title', 'Dashboard')
+
+@section('content')
+<div class="card">
+    <div class="card-body">
+        <h3>Halo Melani 👋</h3>
+        <p>Testing Layout Master Laravel.</p>
+    </div>
+</div>
+@endsection
